7630d0dd87ebbd95209c519a4867127a61a1b813
9vin9 <luvin3994@gmail.com>
2025-10-20 15:01:50 +0900

    타입힌트활용 라우팅

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
-from flask import Flask,url_for
+from flask import Flask
+
 app = Flask(__name__)
 
-# 하드코딩 하지 않고 flask의 url_for 함수를 사용하여 URL을 생성하는 방법
+@app.route('/int/<int:var>')
+def int_type(var):
+    return f'Integer: {var}'
 
-# home page route
-@app.route('/')
-def index():
-    # url_for('index') 호출
-    return f'홈페이지 : {url_for("index")}'
+@app.route('/float/<float:var>')
+def float_var(var: float):
+    return f'Float: {var}'
 
-# user profile page 
-@app.route('/user/<username>')
-def user_profile(username):
-    return f'{username}의 프로필 페이지: {url_for("user_profile", username=username)}'
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath: {subpath}'
 
-# static file route
-@app.route('/static-example')
-def static_example():
-    # url_for ('static', filename='style.css') 호출
-    return f'정적 파일 경로: {url_for("static", filename="style.css")}'
+@app.route('/uuid/<uuid:some_id>')
+def show_uuid(some_id):
+    return f'UUID: {some_id}'
 
-# 절대 url test
-@app.route('/absolute')
-def absolute():
-    # url_for('index', _external=True) 호출
-    return f'절대 URL: {url_for("index", _external=True)}'
-
-#Http와 절대 url test
-@app.route('/https')
-def https():
-    # url_for('index', _external=True, _scheme='https') 호출
-    return f'HTTPS 절대 URL: {url_for("index", _external=True, _scheme="https")}'
-
-#Test: 1. http://127.0.0.1:5000/ -> 홈페이지 url 반환
-#      2. http://127.0.0.1:5000/user/john -> john의 프로필 페이지 url 반환
-#      3. http://127.0.0.1:5000/static-example -> 정적 파일 경로 반환
-#      4. http://127.0.0.1:5000/absolute -> 절대 URL 반환
-#      5. http://127.0.0.1:5000/https -> HTTPS 절대 URL 반환
-
+#test: 1. terminal -> flask run
+#      2. browser -> http://127.0.0.1:5000/int/42
+#      3. browser -> http://127.0.0.1:5000/float/3.14
+#      4. browser -> http://127.0.0.1:5000/path/hello/world
+#      5. browser -> http://127.0.0.1:5000/uuid/123e4567-e89b-12d3-a456-426614174000
