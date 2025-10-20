89a70c4b1a0302b42ccf0673dcc8bf6548ee33d0
9vin9 <luvin3994@gmail.com>
2025-10-20 12:26:06 +0900

    HTTP 메서드 사용

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {username}'
-
-# test: 1. terminal: export FLASK_APP=app.py
-# test: 2. terminal: flask run
-# test: 3. open browser: http://127.0.0.1:5000/user/flask_variable_name
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "Logging in..."
+    else:
+        return "Login Form"
+    
+#test: 1. Send a GET request to /login and verify the response is "Login Form".
+#       terminal command: curl -X GET http://127.0.0.1:5000/login
+#test: 2. Send a POST request to /login and verify the response is "Logging in...".
