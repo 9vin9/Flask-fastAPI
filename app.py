ca0e3379b7975507df9c1ac8037b9f65471ad4df
9vin9 <luvin3994@gmail.com>
2025-10-20 14:16:12 +0900

    URL빌더3

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 from flask import Flask,url_for
 app = Flask(__name__)
 
-# # 홈페이지 경로 /home으로 변경
-# @app.route('/home')
-# def index():
-#     return "홈페이지에 오신 것을 환영합니다!"
+# 하드코딩 하지 않고 flask의 url_for 함수를 사용하여 URL을 생성하는 방법
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {username}'
-
-@app.route('/post/<year>/<month>/<day>')
-def show_post(year, month, day):  
-    return f'Post for {year}/{month}/{day}'
-
+# home page route
 @app.route('/')
 def index():
-    user_url = url_for('show_user_profile', username='John')
-    post_url = url_for('show_post', year='2025', month='10', day='01')
-    return f'User URL: {user_url}<br>Post URL: {post_url}'
+    # url_for('index') 호출
+    return f'홈페이지 : {url_for("index")}'
 
-# test: 1.terminal -> flask run
-#       2. browser -> http://127.0.0.1:5000/
+# user profile page 
+@app.route('/user/<username>')
+def user_profile(username):
+    return f'{username}의 프로필 페이지: {url_for("user_profile", username=username)}'
+
+# static file route
+@app.route('/static-example')
+def static_example():
+    # url_for ('static', filename='style.css') 호출
+    return f'정적 파일 경로: {url_for("static", filename="style.css")}'
+
+# 절대 url test
+@app.route('/absolute')
+def absolute():
+    # url_for('index', _external=True) 호출
+    return f'절대 URL: {url_for("index", _external=True)}'
+
+#Http와 절대 url test
+@app.route('/https')
+def https():
+    # url_for('index', _external=True, _scheme='https') 호출
+    return f'HTTPS 절대 URL: {url_for("index", _external=True, _scheme="https")}'
+
+#Test: 1. http://127.0.0.1:5000/ -> 홈페이지 url 반환
+#      2. http://127.0.0.1:5000/user/john -> john의 프로필 페이지 url 반환
+#      3. http://127.0.0.1:5000/static-example -> 정적 파일 경로 반환
+#      4. http://127.0.0.1:5000/absolute -> 절대 URL 반환
+#      5. http://127.0.0.1:5000/https -> HTTPS 절대 URL 반환
+
