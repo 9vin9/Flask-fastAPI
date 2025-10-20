0faec82f58c21130ebcbb48c222f9e8091d09cb6
9vin9 <luvin3994@gmail.com>
2025-10-20 12:11:57 +0900

    flask_variable_name_fix

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/user/<username>')
 def show_user_profile(username):
     return f'User {username}'
+
+# test: 1. terminal: export FLASK_APP=app.py
+# test: 2. terminal: flask run
+# test: 3. open browser: http://127.0.0.1:5000/user/flask_variable_name
+
