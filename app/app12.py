28a32d4f14738351be30c953d4d9ea41b51e5091
9vin9 <luvin3994@gmail.com>
2025-10-21 12:17:58 +0900

    반복문

---
+++ app/app12.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/fruits')
+def show_fruits():
+    fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+    return render_template('fruits_list.html', fruits=fruits)    
+
+# test: 1. terminal -> flask run
+#       2. open browser -> http://
