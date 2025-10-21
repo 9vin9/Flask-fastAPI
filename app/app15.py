d96c61e7181df2330e3218fdddc6c045edad4774
9vin9 <luvin3994@gmail.com>
2025-10-21 14:45:54 +0900

    정적 파일 다루기

---
+++ app/app15.py
@@ -0,0 +1,8 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/image')
+def get_image():
+   return send_from_directory(app.static_folder, 'image.jpg')
+#   return send_from_directory('static', 'image.jpg')
